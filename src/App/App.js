0f7c5c62c70097d604a3646af575655f2e00c69f
HassanWalid <hassan.walid77@gmail.com>
2024-05-06 22:20:56 +0300

    hassan

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+// import "./App.css";
 // import TimelinePge from "../pages/Project.jsx";
 import { Provider } from "react-redux";
 import store from "../redux/store/store.js";
@@ -8,18 +8,44 @@ import CalendarComp from "../Components/Calendar/CalendarComp.jsx";
 import ProjectPage from "../pages/ProjectPage.jsx";
 import Intro from "../Components/Intro/intro.jsx";
 
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { setCurrentUser } from "../redux/store/slices/currentUserSlice.js";
+
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (localStorage.getItem("id")) {
+      dispatch(
+        setCurrentUser({
+          id: localStorage.getItem("id"),
+          name: localStorage.getItem("name"),
+          picture: localStorage.getItem("picture"),
+          email: localStorage.getItem("email"),
+        })
+      );
+    }
+  }, []);
+
+  const router = createBrowserRouter([
+    { path: "/board", element: <Boardpage></Boardpage> },
+    { path: "/", element: <Intro></Intro> },
+  ]);
   return (
-    <Provider store={store}>
-      <div className="App">
+    // <Provider store={store}>
+    <RouterProvider router={router}>
+      <>
         {/* <TimelinePge></TimelinePge> */}
         {/* <Boardpage></Boardpage> */}
         {/* <Backlog></Backlog>  */}
         {/* <CalendarComp></CalendarComp> */}
-        <ProjectPage></ProjectPage>
+        {/* <ProjectPage></ProjectPage> */}
         {/* <Intro></Intro> */}
-      </div>
-    </Provider>
+      </>
+    </RouterProvider>
+    // </Provider>
   );
 }
 
--- src/Components/Signin/signin.jsx
+++ src/Components/Signin/signin.jsx
@@ -14,7 +14,7 @@ import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import img from "../../images/signImage.gif";
 import { Badge, IconButton, Stack } from "@mui/material";
-import GoogleIcon from '@mui/icons-material/Google';
+import GoogleIcon from "@mui/icons-material/Google";
 import { Google } from "@mui/icons-material";
 import { useState, useEffect } from "react";
 import { googleLogout, useGoogleLogin } from "@react-oauth/google";
@@ -23,31 +23,31 @@ import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { getUserAction } from "../../redux/store/slices/isUserInDBSlice";
 import { setCurrentUser } from "../../redux/store/slices/currentUserSlice";
-import HighlightOffIcon from '@mui/icons-material/HighlightOff';
-import CancelIcon from '@mui/icons-material/Cancel';
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
+import CancelIcon from "@mui/icons-material/Cancel";
+import { useNavigate } from "react-router-dom";
 
 // TODO remove, this demo shouldn't need to reset the theme.
 
 // const defaultTheme = createTheme();
 
-export default function SignIn({closeSignin}) {
-
-  const [user, setUser] = useState(null);//signin state
+export default function SignIn({ closeSignin }) {
+  const [user, setUser] = useState(null); //signin state
 
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const login = useGoogleLogin({
-    onSuccess: (codeResponse) =>{ 
-      setUser(codeResponse)//token
+    onSuccess: (codeResponse) => {
+      setUser(codeResponse); //token
       closeSignin();
+      // navigate("/board");
       //redirect to dashboard
     },
     onError: (error) => console.log("Login Failed:", error),
   });
 
   useEffect(() => {
-
-
     if (user) {
       axios
         .get(
@@ -61,13 +61,19 @@ export default function SignIn({closeSignin}) {
         )
         .then((res) => {
           // console.log(res.data);
-          const userData = {id:res.data.id,name:res.data.name,email:res.data.email,picture:res.data.picture}
-          dispatch(setCurrentUser(userData))
-          localStorage.setItem("id",res.data.id)
-          localStorage.setItem("name",res.data.name)
-          localStorage.setItem("picture",res.data.picture)
-          localStorage.setItem("email",res.data.email)
+          const userData = {
+            id: res.data.id,
+            name: res.data.name,
+            email: res.data.email,
+            picture: res.data.picture,
+          };
+          dispatch(setCurrentUser(userData));
+          localStorage.setItem("id", res.data.id);
+          localStorage.setItem("name", res.data.name);
+          localStorage.setItem("picture", res.data.picture);
+          localStorage.setItem("email", res.data.email);
           dispatch(getUserAction(res.data));
+          navigate("/board");
         })
         .catch((err) => console.log(err));
     }
@@ -83,74 +89,82 @@ export default function SignIn({closeSignin}) {
   };
 
   return (
-    
-      <Grid 
-        container
-        sx={{ width: "60vw", borderRadius: "13px" ,height:"60vh"}}
-        > 
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={4}
-          sx={
-            {
-              backgroundImage: `url(${img})`,
-              backgroundRepeat: "no-repeat",
-              backgroundColor: (t) =>
-                t.palette.mode === "light"
+    <Grid
+      container
+      sx={{ width: "60vw", borderRadius: "13px", height: "60vh" }}
+    >
+      <Grid
+        item
+        xs={false}
+        sm={4}
+        md={4}
+        sx={{
+          backgroundImage: `url(${img})`,
+          backgroundRepeat: "no-repeat",
+          backgroundColor: (t) =>
+            t.palette.mode === "light"
               ? t.palette.grey[50]
               : t.palette.grey[900],
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-            }
-          }
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+
+      <Grid
+        item
+        xs={12}
+        sm={8}
+        md={8}
+        component={Paper}
+        elevation={6}
+        square
+        height={"100%"}
+      >
+        <Stack height={"100%"} alignItems={"center"}>
+          <IconButton
+            sx={{ width: "fit-content", alignSelf: "end" }}
+            onClick={closeSignin}
+          >
+            <CancelIcon></CancelIcon>
+          </IconButton>
+
+          <Avatar sx={{ bgcolor: "#060F27" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+
+          <TextField
+            margin="normal"
+            required
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            sx={{ width: "70%" }}
           />
-        
-        <Grid item  xs={12} sm={8} md={8} component={Paper} elevation={6} square height={"100%"}>
-            <Stack
-            height={"100%"}
-            alignItems={"center"}
-            >
-              <IconButton sx={{width:"fit-content",alignSelf:"end"}} onClick={closeSignin}><CancelIcon ></CancelIcon></IconButton>
 
-              <Avatar sx={{ bgcolor: "#060F27"}}>
-                <LockOutlinedIcon />
-              </Avatar>
-              
-              <Typography component="h1" variant="h5">
-                Sign in
-              </Typography>
-              
-                <TextField
-                  margin="normal"
-                  required
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                  autoFocus
-                  sx={{width:"70%"}}
-                />
-
-                <Button
-                  // type="submit"
-                  variant="contained"
-                  sx={{ mt: 3, mb: 2,width:"60%" ,backgroundColor:"#060F27"}}
-                >
-                  Sign In
-                </Button>
-                <Button
-                  onClick={login}
-                  variant="contained"
-                  startIcon={<Google></Google>}
-                  sx={{ mt: 3, mb: 2,width:"60%",backgroundColor:"#060F27" }}
-                >sign in with google
-                </Button>
-              </Stack>
-        </Grid>
-        
+          <Button
+            // type="submit"
+            variant="contained"
+            sx={{ mt: 3, mb: 2, width: "60%", backgroundColor: "#060F27" }}
+          >
+            Sign In
+          </Button>
+          <Button
+            onClick={login}
+            variant="contained"
+            startIcon={<Google></Google>}
+            sx={{ mt: 3, mb: 2, width: "60%", backgroundColor: "#060F27" }}
+          >
+            sign in with google
+          </Button>
+        </Stack>
       </Grid>
+    </Grid>
     // </ThemeProvider>
   );
 }
